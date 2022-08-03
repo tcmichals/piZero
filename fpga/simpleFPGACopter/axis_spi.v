9281b12e57041056782eb0e4998c25d11f0dbf53
Tim Michals <tcmichals@yahoo.com>
2022-08-03 17:11:26 -0500

    Updated spi axis not to use a buffer

--- fpga/simpleFPGACopter/axis_spi.v
+++ fpga/simpleFPGACopter/axis_spi.v
@@ -1,85 +1,52 @@
 module axis_spi
 (
-    input wire i_clk,
-    input wire i_reset,
+    input wire axis_aclk,
+    input wire axis_aresetn,
 
     input wire i_spi_clk,
     output wire o_spi_miso,
     input wire i_spi_mosi,
     input wire i_spi_cs,
 
-    input wire [7:0]  i_axis_tdata,
-    input wire        i_axis_tkeep,
-    input wire        i_axis_tvalid,
-    output wire        i_axis_tready,
-    input wire        i_axis_tlast,
-    input wire        i_axis_tuser,
+    input wire [7:0]  s_axis_tdata,
+    input wire        s_axis_tkeep,
+    input wire        s_axis_tvalid,
+    output wire        s_axis_tready,
+    input wire        s_axis_tlast,
+    input wire        s_axis_tuser,
 
-    output wire [7:0]  o_axis_tdata,
-    output wire        o_axis_tkeep,
-    output wire        o_axis_tvalid,
-    input wire        o_axis_tready,
-    output wire        o_axis_tlast,
-    output wire        o_axis_tuser
+    output wire [7:0]  m_axis_tdata,
+    output wire        m_axis_tkeep,
+    output wire        m_axis_tvalid,
+    input wire        m_axis_tready,
+    output wire        m_axis_tlast,
+    output wire        m_axis_tuser
 );
 
-
-assign o_axis_tkeep = 1'b1;
-assign o_axis_tuser = 1'b1;
-
-wire rt_fifo_empty;
-wire rx_spi_byte_ready;
-wire [7:0] rx_spi_byte;
-
-wire i_axis_tready;
-
-assign  rx_spi_byte_ready = ~rt_fifo_empty & o_axis_tready;
-assign o_axis_tlast = rt_fifo_empty;
-assign o_axis_tvalid = rx_spi_byte_ready;
-
-FIFO spi_rx (.i_Rst_L(i_reset),
-            .i_Clk(i_clk),
-            .i_Wr_DV(spi_rx_ready),
-            .i_Wr_Data(spi_rx_byte),
-            .i_AF_Level(),
-            .o_AF_Flag(),
-            .o_Full(),
-            .i_Rd_En(),
-            .o_Rd_DV(rx_spi_byte_ready),
-            .o_Rd_Data(o_axis_tdata),
-            .i_AE_Level(),
-            .o_AE_Flag(),
-            .o_Empty(rx_fifo_empty));
-
-wire tx_fifo_full;
-wire tx_fifo_empty;
-assign i_axis_tready = ~tx_fifo_full;
-assign spi_tx_out = ~tx_fifo_empty;
-wire [0:7] spi_tx_byte;
-
-
-FIFO spi_tx (.i_Rst_L(i_reset),
-            .i_Clk(i_clk),
-            .i_Wr_DV(i_axis_tvalid),
-            .i_Wr_Data(i_axis_tdata),
-            .i_AF_Level(),
-            .o_AF_Flag(),
-            .o_Full(tx_fifo_full),
-            .i_Rd_En(),
-            .o_Rd_DV(spi_tx_out),
-            .o_Rd_Data(spi_tx_byte),
-            .i_AE_Level(),
-            .o_AE_Flag(),
-            .o_Empty(tx_fifo_empty));
+wire rx_ready_spi;
 
 
 
-SPI_Slave spi(  .i_Rst_L(i_reset),
-                .i_Clk(i_clk),
-                .o_RX_DV(spi_rx_ready),
-                .o_RX_Byte(spi_rx_byte),
-                .i_TX_DV(spi_tx_out),
-                .i_TX_Byte(spi_tx_byte),
+
+wire axis_tready;
+
+assign m_axis_tlast = 1'b1;;
+assign m_axis_tuser = 1'b1;
+assign m_axis_tkeep = 1'b1;
+
+
+assign m_axis_tvalid = rx_byte_spi & m_axis_tready;
+assign s_axis_tready = rx_ready_spi & 
+
+
+            
+
+SPI_Slave spi(  .i_Rst_L(axis_aresetn),
+                .i_Clk(axis_aclk),
+                .o_RX_DV(rx_ready_spi),
+                .o_RX_Byte(m_axis_tdata),
+                .i_TX_DV(tx_write_spi),
+                .i_TX_Byte(s_axis_tdata),
                 .i_SPI_Clk(i_spi_clk),
                 .o_SPI_MISO(o_spi_miso),
                 .i_SPI_MOSI(i_spi_mosi),
---
+++ fpga/simpleFPGACopter/tb_axis_spi.v
@@ -0,0 +1,135 @@
+///////////////////////////////////////////////////////////////////////////////
+// Description:       Simple test bench for SPI Master and Slave modules
+///////////////////////////////////////////////////////////////////////////////
+
+module tb_axis_spi();
+  
+parameter SPI_CLK_DELAY = 20;  // 2.5 MHz
+
+parameter MAIN_CLK_DELAY = 2;  // 25 MHz
+
+reg reset = 1'b1;
+reg clk = 1'b1;
+reg spi_clk = 1'b1;
+reg spi_ss = 1'b1;
+reg spi_mosi = 1'b0;
+wire spi_miso;
+
+wire [7:0]s_axis_tdata;
+wire s_axis_tkeep;
+wire  s_axis_tvalid;
+reg  s_axis_tready = 1'b1;
+wire s_axis_tlast;
+wire  s_axis_tuser;
+
+reg [7:0] m_axis_tdata;
+reg  m_axis_tkeep;
+reg  m_axis_tvalid;
+wire m_axis_tready;
+reg  m_axis_tlast;
+reg  m_axis_tuser;
+
+
+  // Clock Generators:
+  always #(MAIN_CLK_DELAY) clk = ~clk;
+  // Clock Generators:
+  always #(SPI_CLK_DELAY) spi_clk = ~spi_clk;
+
+  axis_spi spi(
+            .axis_aclk(clk),
+    .axis_aresetn(reset),
+
+    .i_spi_clk(spi_clk),
+    .o_spi_miso(spi_miso),
+    .i_spi_mosi(spi_mosi),
+    .i_spi_cs(spi_ss),
+
+    .s_axis_tdata(m_axis_tdata),
+    .s_axis_tkeep(m_axis_tkeep),
+    .s_axis_tvalid(m_axis_tvalid),
+    .s_axis_tready(m_axis_tready),
+    .s_axis_tlast(m_axis_tlast),
+    .s_axis_tuser(m_axis_tuser),
+
+    .m_axis_tdata(s_axis_tdata),
+    .m_axis_tkeep(s_axis_tkeep),
+    .m_axis_tvalid(s_axis_tvalid),
+    .m_axis_tready(s_axis_tready),
+    .m_axis_tlast(s_axis_tlast),
+    .m_axis_tuser(s_axis_tuser)
+  );
+
+
+task send_spi( input [7:0] data);
+   reg spi_out_bit;
+   reg [2:0]r_TX_Bit_Count;  // Send MSb first
+   r_TX_Bit_Count <= 3'b111; 
+      $display("send spi 0x%H", data);
+
+    @(negedge spi_clk) begin
+      spi_mosi = data[r_TX_Bit_Count];
+      spi_ss <= 1'b0;
+      $display("%dth bit %B", r_TX_Bit_Count, data[r_TX_Bit_Count]);
+    end
+      r_TX_Bit_Count <= r_TX_Bit_Count - 1;
+
+    @(negedge spi_clk) begin
+      spi_mosi = data[r_TX_Bit_Count];
+      $display("%dth bit %B", r_TX_Bit_Count, data[r_TX_Bit_Count]);
+    end
+      r_TX_Bit_Count <= r_TX_Bit_Count - 1;
+
+    @(negedge spi_clk) begin
+      spi_mosi = data[r_TX_Bit_Count];
+      $display("%dth bit %B", r_TX_Bit_Count, data[r_TX_Bit_Count]);
+    end
+      r_TX_Bit_Count <= r_TX_Bit_Count - 1;
+
+    @(negedge spi_clk) begin
+        spi_mosi = data[r_TX_Bit_Count];
+    end
+      r_TX_Bit_Count <= r_TX_Bit_Count - 1;        
+
+    @(negedge spi_clk) begin
+        spi_mosi = data[r_TX_Bit_Count];
+    end
+      r_TX_Bit_Count <= r_TX_Bit_Count - 1;        
+
+    @(negedge spi_clk) begin
+        spi_mosi <= data[r_TX_Bit_Count];
+    end
+      r_TX_Bit_Count <= r_TX_Bit_Count - 1;        
+
+    @(negedge spi_clk) begin
+        spi_mosi = data[r_TX_Bit_Count];
+    end
+      r_TX_Bit_Count <= r_TX_Bit_Count - 1;
+
+     @(negedge spi_clk) begin
+        spi_mosi = data[r_TX_Bit_Count];
+    end
+      r_TX_Bit_Count <= r_TX_Bit_Count - 1;
+
+     @(negedge spi_clk);
+        spi_ss <= 1'b1;
+        spi_mosi <= 1'b0;
+endtask
+
+
+  initial begin
+    $dumpfile("tb_axis_spi.vcd");
+	  $dumpvars(0,tb_axis_spi);
+    #2 reset <= 1'b0;
+    #10 reset <= 1'b1;
+
+    #40 send_spi(8'hA5);
+    #40 send_spi(8'hAA);
+    #40 $finish;	
+  	 $display("Done");
+    end
+
+
+endmodule // SPI_Slave
+
+
+
